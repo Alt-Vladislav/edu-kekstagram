2ab014374e61c5b7b37f97a944b0cc7a4124047e
Vladislav <vlad.4sa@gmail.com>
2024-03-30 21:01:18 +0700

    Валидация хештегов и описания сделана

---
+++ js/upload-new-photo-validation.js
@@ -0,0 +1,60 @@
+import { getHashtags, checkFirstSymbol, checkLength, checkSpacesBetween, checkRepeat } from './functions-validation';
+
+const MAX_LENGTH_HASHTAG = 20;
+const MAX_LENGTH_DESCRIPTION = 140;
+const MAX_NUMBER_HASHTAGS = 5;
+
+let hashtags = [];
+let errorTextHashtegs = 'ошибка валидации хештега';
+const regularExpression = /^#[a-zа-яё0-9]{1,20}$/;
+
+const checkHashtagsValidity = (hashtegsString) => {
+  hashtags = getHashtags(hashtegsString);
+
+  if (checkFirstSymbol(hashtegsString)) {
+    errorTextHashtegs = 'хэштег должен начинаеться c символа # (решётка)';
+    return false;
+  } else if (checkSpacesBetween(hashtags)) {
+    errorTextHashtegs = 'хэштеги должны разделяться пробелами';
+    return false;
+  } else if (hashtags.some((element) => element.trimEnd() === '#')) {
+    errorTextHashtegs = 'хеш-тег не может состоять только из одной решётки';
+    return false;
+  } else if (hashtags.some((element) => checkLength(element, MAX_LENGTH_HASHTAG))) {
+    errorTextHashtegs = `максимальная длина одного хэштега ${MAX_LENGTH_HASHTAG} символов`;
+    return false;
+  } else if (hashtags.some((element) => !regularExpression.test(element.trimEnd()))) {
+    errorTextHashtegs = 'хештег содержит недопустимые символы';
+    return false;
+  } else if (checkRepeat(hashtags)) {
+    errorTextHashtegs = 'один и тот же хэштег не может быть использован дважды';
+    return false;
+  } else if (hashtags.length > MAX_NUMBER_HASHTAGS) {
+    errorTextHashtegs = `нельзя указать больше ${MAX_NUMBER_HASHTAGS} хэштегов`;
+    return false;
+  }
+
+  return true;
+};
+
+const checkDescriptionValidity = (value) => !checkLength(value.trimEnd(), MAX_LENGTH_DESCRIPTION);
+
+const getErrorMessage = () => errorTextHashtegs;
+
+
+const initUploadFormValidation = (form, hashtagsInput, descriptionInput) => {
+  const prisitineConfig = {
+    classTo: 'img-upload__field-wrapper',
+    errorTextParent: 'img-upload__field-wrapper',
+    errorTextTag: 'div',
+    errorTextClass: 'img-upload__field-wrapper--error'
+  };
+  const pristine = new Pristine(form, prisitineConfig);
+
+
+  pristine.addValidator(hashtagsInput, checkHashtagsValidity, getErrorMessage);
+  pristine.addValidator(descriptionInput, checkDescriptionValidity, `не больше ${MAX_LENGTH_DESCRIPTION} символов`);
+};
+
+
+export { initUploadFormValidation };
--- js/upload-new-photo.js
+++ js/upload-new-photo.js
@@ -1,7 +1,12 @@
+import { initUploadFormValidation } from './upload-new-photo-validation';
+
 const uploadFormElement = document.querySelector('.img-upload__form');
 const uploadFileInputElement = uploadFormElement.querySelector('#upload-file');
 const popupEditorContainerELement = uploadFormElement.querySelector('.img-upload__overlay');
 const cancelButtonElement = popupEditorContainerELement.querySelector('#upload-cancel');
+const hashtagsInputElement = popupEditorContainerELement.querySelector('.text__hashtags');
+const descriptionInputElement = popupEditorContainerELement.querySelector('.text__description');
+
 
 const onEscKeydown = (evt) => {
   if (evt.key === 'Escape') {
@@ -26,13 +31,22 @@ function closeUploadForm () {
 
 
 const initUploadForm = () => {
+  initUploadFormValidation(uploadFormElement, hashtagsInputElement, descriptionInputElement);
+
   uploadFileInputElement.addEventListener('change', () => {
     popupEditorContainerELement.classList.remove('hidden');
     document.body.classList.add('modal-open');
 
     document.addEventListener('keydown', onEscKeydown);
     cancelButtonElement.addEventListener('click', onCancelButtonCleack);
+
+    //тут будет добавление события на sumbit. Надо будет сбросить если поле хештегов и поле описания вся из пробелов. использовать метод .trim()
+    // hashtagsInputElement.addEventListener('input', () => {
+    //   var valid = pristine.validate(hashtagsInputElement);
+    //   console.log(valid);
+    // });
   });
 };
+
 
 export { initUploadForm };
